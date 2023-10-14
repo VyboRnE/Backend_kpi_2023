0c55062e24369095b1ee38974819a53fefdb7b93
Yevhenii Kozlenko <kozlenko4380@gmail.com>
2023-10-14 22:13:05 +0300

    Оновлено файл views.py. Створено ендпоінт відповідно до завдання лабораторної роботи.

--- mainModule/views.py
+++ mainModule/views.py
@@ -1,10 +1,16 @@
 from flask import Flask
+import json, time
+
 app = Flask(__name__)
 
 @app.route("/")
 def base():
     return "<p>It works:)</p>"
 
-@app.route("/healthcheck")
+@app.route("/healthcheck", methods=['GET'])
 def healthcheck():
-    return "<p>Healthcheck</p>"
+    healthcheck_data = {'Page': 'Healthcheck', 
+                        'Status': 'Service is working.', 
+                        'Time': time.asctime(time.localtime(time.time()))}
+    healthcheck_json = json.dumps(healthcheck_data)
+    return healthcheck_json, 200
